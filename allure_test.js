b68dab30300156f4ddf53696a114c17669e53de8
Akatsuki_Miao <630495033@qq.com>
2020-11-12 19:13:20 +0800

    文件更改

--- allure_test.js
+++
@@ -1,7 +0,0 @@
-// const allure = codeceptjs.container.plugins('allure')
-Feature("我在百度");
-Scenario("百度验收标准",(I) => {
-    I.amOnPage("https://www.baidu.com/")
-    // I.see("动感超人")
-    // I.click("//input[@value='百度一下']")
-})
